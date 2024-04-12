42589302797a883c483e9de140c27e22c2ef9e94
nquirogac <nquirogac@unal.edu.co>
2024-04-12 15:21:38 -0500

    new branch

--- LogsApplication/settings.py
+++ LogsApplication/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'LogsApplication.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
+        'NAME': 'banco_los_alpes',
+        'USER': 'admin',
+        'PASSWORD': 'isis2503',
+        'HOST': '10.128.0.52',
         'PORT': '',
     }
 }
