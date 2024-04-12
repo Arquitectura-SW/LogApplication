ffc87941e5e61476ef941d8294e4b76bd580b00e
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-04-12 18:14:22 -0500

    logs model

--- logs/models.py
+++ logs/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from uuid import uuid
+from uuid import uuid4
 
 class Log(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     level = models.CharField(max_length=10)
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
